88a8248588afcc71cc1fc3b3f44a670b638546d8
Misha shimchishin git config --global user,email mykhailo.shimchishin@icloud,com <you@example.com>
2025-02-12 15:10:23 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const baseCost = 40;
+  const discountFor3Days = 20;
+  const discountFor7Days = 50;
+
+  if (days >= 7) {
+    return baseCost * days - discountFor7Days;
+  } else if (days >= 3) {
+    return baseCost * days - discountFor3Days;
+  } else {
+    return baseCost * days;
+  }
 }
 
 module.exports = calculateRentalCost;
